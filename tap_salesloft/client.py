4aa08685e3fd9f220c5aa3466c8a63c623011be8
Boris Ralichkov <boris.ralichkov@dext.com>
2025-11-21 13:12:10 +0200

    Enable `end_date` (unfinished)

--- tap_salesloft/client.py
+++ tap_salesloft/client.py
@@ -55,6 +55,7 @@ class SalesloftStream(RESTStream):
         if self.replication_key:
             params['sort_direction'] = 'asc'
             params['sort_by'] = self.replication_key
+            params[f'{self.replication_key}[lt]'] = self.config.get('end_date')
             if next_page_token is not None:
                 params[f'{self.replication_key}[gt]'] = next_page_token
             else:
