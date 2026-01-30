3ca0b0c821c669323e8a9c25fa53bc260044b7b5
Boris Ralichkov <git@ralichkov.com>
2026-01-30 13:43:39 +0200

    fixup: `TranscriptionSentences`: add surrogate key

--- tap_salesloft/streams/transcription_sentences.py
+++ tap_salesloft/streams/transcription_sentences.py
@@ -1,3 +1,6 @@
+import hashlib
+import json
+
 from tap_salesloft.client import SalesloftStream
 from tap_salesloft.streams import TranscriptionsStream
 
@@ -18,24 +21,42 @@ class TranscriptionSentencesStream(SalesloftStream):
     path = '/v2/transcriptions/{transcription_id}/sentences'
     # ID is the same value throughout the entire conversation.
     # We need an additional PK but `order_number` isn't present in older data.
-    primary_keys = ['id', 'start_time', 'end_time']
+    primary_keys = ['__pk']
     state_partitioning_keys = []
 
     parent_stream_type = TranscriptionsStream
 
     schema = PropertiesList(
-        Property('id', StringType, required=True, description='Transcription Id'),
+        Property('__pk', StringType, required=True, description='Surrogate key'),
+        Property('id', UUIDType, required=True, description='Transcription Id'),
         Property('start_time', DecimalType, required=True, description='Sentence start time'),
         Property('end_time', DecimalType, required=True, description='Sentence end time'),
-        Property('order_number', IntegerType, required=True, description='Sentence order number'),
+        Property('order_number', IntegerType, required=False, description='Sentence order number'),
         Property('recording_attendee_id', UUIDType, required=True, description='Attendee id'),
-        Property('text', StringType, description='Sentence text'),
+        Property('text', StringType, required=True, description='Sentence text'),
         Property(
             'conversation',
             ObjectType(
-                Property('id', StringType),
-                Property('_href', StringType),
+                Property('id', StringType, required=True),
+                Property('_href', StringType, required=True),
             ),
+            required=True,
             description='Reference to the Conversation',
         ),
     ).to_dict()
+
+    def post_process(self, row, context=None) -> dict:
+        row['__pk'] = hashlib.sha256(
+            json.dumps(
+                {
+                    'id': row.get('id'),
+                    'start_time': str(row.get('start_time')),
+                    'end_time': str(row.get('end_time')),
+                    'order_number': row.get('order_number'),
+                    'recording_attendee_id': row.get('recording_attendee_id'),
+                },
+                sort_keys=True,
+                default=str,
+            ).encode('utf-8')
+        ).hexdigest()
+        return row
